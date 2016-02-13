4197b049f9bef6e856b6886a6ea78cf6eafdb1c3
Lemon <lemon@thefpl.us>
2016-02-12 22:42:15 -0600

    Wheeeeeeeee

--- js/partials/_photos.js
+++ js/partials/_photos.js
@@ -72,103 +72,69 @@ var wikiHow = [
     slug: "Be-Emotionless"
   },
   {
-    pic: "http://40.media.tumblr.com/723d73b71c830e40e6636375989c4da5/tumblr_nzbw2vc2q11tzofdno1_1280.jpg",
     slug: "Understand-Heterosexual-People"
   },
   {
-    pic: "http://pad1.whstatic.com/images/thumb/6/6e/Cheat-On-a-Test-Step-7-Version-3.jpg/728px-Cheat-On-a-Test-Step-7-Version-3.jpg",
     slug: "Cheat-On-a-Test"
   },
   {
-    pic: "http://pad1.whstatic.com/images/thumb/7/75/483861-1.jpg/760px-483861-1.jpg",
     slug: "Live-in-Peace"
   },
   {
-    pic: "http://41.media.tumblr.com/cd17db2ca7c4ee33b75b66890e919257/tumblr_nylrxppy4o1tzofdno1_1280.jpg",
     slug: "Lose-Your-Virginity-Without-Pain-(Girls)"
   },
   {
-     pic: "http://pad2.whstatic.com/images/thumb/d/df/Get-Rid-of-an-Unwanted-Erection-Step-10-Version-2.jpg/760px-Get-Rid-of-an-Unwanted-Erection-Step-10-Version-2.jpg",
     slug: "Get-Rid-of-an-Unwanted-Erection"
   },
   {
-     pic: "img/pics/Resist-Sexual-Temptation.jpg",
     slug: "Resist-Sexual-Temptation"
   },
   {
-     pic: "http://pad3.whstatic.com/images/thumb/6/69/Tell-a-Girl-You-Like-Her-Step-2.jpg/728px-Tell-a-Girl-You-Like-Her-Step-2.jpg",
     slug: "Tell-a-Girl-You-Like-Her"
   },
   {
-     pic: "http://pad1.whstatic.com/images/thumb/0/00/Own-and-Love-a-Pit-Bull-Dog-Step-6.jpg/728px-Own-and-Love-a-Pit-Bull-Dog-Step-6.jpg",
     slug: "Own-and-Love-a-Pit-Bull-Dog"
   },
   {
-     pic: "http://pad3.whstatic.com/images/thumb/8/87/Make-Someone-You-Met-on-MSN-Love-You-Step-10.jpg/728px-Make-Someone-You-Met-on-MSN-Love-You-Step-10.jpg",
     slug: "Make-Someone-You-Met-on-MSN-Love-You"
   },
   {
-     pic: "http://pad3.whstatic.com/images/thumb/9/9e/Enjoy-Phone-Sex-Step-4-Version-2.jpg/728px-Enjoy-Phone-Sex-Step-4-Version-2.jpg",
     slug: "Enjoy-Phone-Sex"
   },
   {
-     pic: "http://36.media.tumblr.com/075e136ef1885402ad9ac6480ff78224/tumblr_nvf4w8uqTm1tzofdno1_1280.jpg",
     slug: "Raise-a-Child"
   },
   {
-     pic: "http://pad1.whstatic.com/images/thumb/f/fd/Build-a-Bloodstone-Circle-Step-7.jpg/728px-Build-a-Bloodstone-Circle-Step-7.jpg",
-    slug: "Build-a-Bloodstone-Circle"
-  },
-  {
-     pic: "http://pad3.whstatic.com/images/thumb/5/5d/Love-Your-Girlfriend-Step-11-Version-3.jpg/728px-Love-Your-Girlfriend-Step-11-Version-3.jpg",
     slug: "Love-Your-Girlfriend"
   },
   {
-     pic: "http://pad1.whstatic.com/images/thumb/e/ee/Win-a-Tickle-War-Step-6.jpg/670px-Win-a-Tickle-War-Step-6.jpg",
     slug: "Win-a-Tickle-War"
   },
   {
-     pic: "http://pad3.whstatic.com/images/thumb/4/46/Talk-to-Girls-as-a-Teen-Boy-Step-5.jpg/760px-Talk-to-Girls-as-a-Teen-Boy-Step-5.jpg",
     slug: "Talk-to-Girls-as-a-Teen-Boy"
   },
   {
-     pic: "http://40.media.tumblr.com/66a72e89acfa128b9fb0625d9d726ab9/tumblr_nrclpqShjE1tzofdno1_1280.jpg",
     slug: "Survive-a-Charging-Elephant"
   },
   {
-     pic: "http://41.media.tumblr.com/5fbaa5c0793456a33ccd8ee1f4f9dd3d/tumblr_nqxb6jUow51tzofdno1_1280.jpg",
-    slug: "Get-the-Hiccups"
-  },
-  {
-     pic: "http://pad3.whstatic.com/images/thumb/a/a6/Stop-Staring-at-a-Girl%27s-Boobs-Step-8.jpg/728px-Stop-Staring-at-a-Girl%27s-Boobs-Step-8.jpg",
     slug: "Stop-Staring-at-a-Girl's-Boobs"
   },
   {
-     pic: "http://pad2.whstatic.com/images/thumb/4/41/Look-Like-a-Computer-Cracker-Step-5-Version-2.jpg/728px-Look-Like-a-Computer-Cracker-Step-5-Version-2.jpg",
-    slug: "Look-Like-a-Computer-Cracker"
-  },
-  {
-     pic: "http://pad3.whstatic.com/images/thumb/2/24/Make-Friends-Online-Step-03.jpg/670px-Make-Friends-Online-Step-03.jpg",
     slug: "Make-Friends-Online"
   },
   {
-     pic: "http://41.media.tumblr.com/e7d48088109f7f33fecc0301de78495a/tumblr_nfx9cakWVQ1tzofdno1_1280.jpg",
     slug: "Get-a-Gay-Male-Friend-(for-Girls)"
   },
   {
-     pic: "http://40.media.tumblr.com/7a989ea34507e3a833c8a161d77b8686/tumblr_n49shafaCz1tzofdno1_1280.jpg",
     slug: "Reason"
   },
   {
-     pic: "img/pics/not-do-drugs.jpg",
     slug: "Not-Do-Drugs"
   },
   {
-     pic: "img/pics/Make-Your-Boyfriend-Break-up-With-You.jpg",
     slug: "Make-Your-Boyfriend-Break-up-With-You"
   },
   {
-     pic: "img/pics/Give-Someone-a-Hickey.jpg",
     slug: "Give-Someone-a-Hickey"
   },
   {
