6eb5dff1a44e25b0d2185e4f7dd46dcfa8a15c60
Lemon <AhoyLemon@users.noreply.github.com>
2018-03-22 13:22:03 -0500

    cache v104b

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,41 @@
 'use strict';
 
-const cacheName = 'v1.01a';
+const PRECACHE = 'precache-v104b';
+const RUNTIME = 'runtime';
 
-self.addEventListener('install', e => {
-  // once the SW is installed, go ahead and fetch the resources
-  // to make this work offline
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/index.html',
-        '/manifest.json',
-        '/css/damn.css',
-        '/js/min/damn.vue.min.js',
-        'js/libraries/vue.js',
-        'js/libraries/vue.min.js',
-        '/svg/offline-dog.svg',
-        '/offline.html'
-      ]).then(() => self.skipWaiting());
-    })
+// A list of local resources we always want to be cached.
+const PRECACHE_URLS = [
+  './', // Alias for index.html
+  '/index.html',
+  '/manifest.json',
+  '/css/damn.css',
+  '/js/min/damn.vue.min.js',
+  'js/libraries/vue.js',
+  'js/libraries/vue.min.js',
+  '/svg/offline-dog.svg',
+  offlineUrl
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
   );
+});
+
+self.addEventListener('fetch', function(event) {
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(
+      fetch(event.request.url).catch(error => {
+        return caches.match(offlineUrl);
+      })
+    );
+  } else {
+    event.respondWith(caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+      })
+    );
+  }
 });
