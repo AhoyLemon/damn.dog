c19e35ea3d9924500eb18fc13c5121403d9f3378
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2020-06-07 17:24:17 -0500

    New service worker from serviceworke.rs

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,50 @@
-//This is the "Offline page" service worker
+const cacheName = 'v2.17';
+const offlineUrl = '/offline.html';
+const offlineFiles = [
+  '/offline.html',
+  '/manifest.json',
+  '/css/damn.css',
+  'https://fonts.googleapis.com/css?family=Roboto+Slab:100,400,300,700|Roboto:400,300,700,400italic',
+  '/js/libraries/jquery-3.3.1.js',
+  '/js/js/libraries/vue.js',
+  '/audio/party.mp3',
+  '/audio/venga.mp3'
+];
 
-//Install stage sets up the offline page in the cache and opens a new cache
+
 self.addEventListener('install', function(event) {
-  var offlinePage = new Request('offline.html');
+  var offlineRequest = new Request(offlineFiles);
   event.waitUntil(
-    fetch(offlinePage).then(function(response) {
-      return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached offline page during Install '+ response.url);
-        return cache.put(offlinePage, response);
+    fetch(offlineRequest).then(function(response) {
+      return caches.open('offline').then(function(cache) {
+        console.log('[oninstall] Cached offline page', response.url);
+        return cache.put(offlineRequest, response);
       });
-  }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  // Only fall back for HTML documents.
+  var request = event.request;
+  // && request.headers.get('accept').includes('text/html')
+  if (request.method === 'GET') {
+    // `fetch()` will use the cache when possible, to this examples
+    // depends on cache-busting URL parameter to avoid the cache.
+    event.respondWith(
+      fetch(request).catch(function(error) {
+        // `fetch()` throws an exception when the server is unreachable but not
+        // for valid HTTP responses, even `4xx` or `5xx` range.
+        console.error(
+          '[onfetch] Failed. Serving cached offline fallback ' +
+          error
+        );
+        return caches.open('offline').then(function(cache) {
+          return cache.match(offlineUrl);
+        });
+      })
+    );
+  }
+  // Any other handlers come here. Without calls to `event.respondWith()` the
+  // request will be handled without the ServiceWorker.
 });
